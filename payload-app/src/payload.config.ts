cf433a0bbd20065ac7d61e74d946a12d984a4a61
Hamza Zaaboul <hamzzaaboul@gmail.com>
2024-10-20 21:32:18 +0200

    Payload CMS Setup

--- payload-app/src/payload.config.ts
+++ payload-app/src/payload.config.ts
@@ -9,12 +9,27 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 
+/**
+ * @payload
+ */
+import { en } from '@payloadcms/translations/languages/en'
+import { fr } from '@payloadcms/translations/languages/fr'
+import { seoPlugin } from '@payloadcms/plugin-seo'
+import { s3Storage } from '@payloadcms/storage-s3'
+
+
+
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
   admin: {
     user: Users.slug,
+    autoLogin: {
+      email: 'zelenionzelenion@gmail.com',
+      password: 'MarieCurie!',
+      prefillOnly: true,
+    },
     importMap: {
       baseDir: path.resolve(dirname),
     },
@@ -25,6 +40,23 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
+  i18n: {
+    supportedLanguages: { en, fr },
+  },
+  localization: {
+    locales: [
+      {
+        label: 'English',
+        code: 'en',
+      },
+      {
+        label: 'Français',
+        code: 'fr',
+      },
+    ],
+    defaultLocale: 'fr',
+    fallback: true,
+  },
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
@@ -32,6 +64,28 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    seoPlugin({
+      collections: [],
+      uploadsCollection: 'media',
+      generateTitle: ({ doc }: any) => `Website.com — ${doc.title}`,
+      generateDescription: ({ doc }: any) => doc.excerpt,
+    }),
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        },
+      },
+      bucket: process.env.S3_BUCKET as string,
+      config: {
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+        },
+        region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
   ],
 })
