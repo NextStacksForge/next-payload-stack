a534c4022623051752d3f7227b072a7ac73e5956
Hamza Zaaboul <hamzzaaboul@gmail.com>
2024-10-20 20:06:32 +0200

    Testing Setup

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image aria-hidden src="https://nextjs.org/icons/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
+          Go to nextjs.org → re
         </a>
       </footer>
     </div>
