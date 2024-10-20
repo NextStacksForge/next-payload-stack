aad8d68b454abec672a41ac656c291df0f932189
Hamza Zaaboul <hamzzaaboul@gmail.com>
2024-10-20 22:11:37 +0200

    All Readme Update

--- web-app/sentry.client.config.ts
+++ web-app/sentry.client.config.ts
@@ -2,13 +2,15 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://593d227697064767927746e5ffd920a5@o4508149469413376.ingest.de.sentry.io/4508149727363152",
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -23,4 +25,4 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-})
+});
